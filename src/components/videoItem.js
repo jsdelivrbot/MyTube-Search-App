cf33417a7816c43afa3d07289556deca694ac7f2
Vinhlee95 <lethanhvinh95@gmail.com>
2018-01-02 23:10:32 +0200

    save

--- src/components/videoItem.js
+++ src/components/videoItem.js
@@ -9,6 +9,7 @@ const VideoItem = ({video, onVideoSelect}) => {
 
    return(
       <li className = 'list-group-item' onClick = {() => onVideoSelect(video)}>
+      
          <div className = 'video-list media'>
             <div className = 'media-left'>
                <img className = 'media-object' src = {imageThumbnail} />
